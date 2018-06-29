afb92bf5c9cdfb110e865ae43813f122f29c348a
violerine <viosefferine@gmail.com>
2018-06-29 11:25:17 +0700

    room view

---
+++ game/src/components/Room.vue
@@ -0,0 +1,97 @@
+<template>
+  <div class="body-background">
+    <div class="register" style="background-color:transparent; width:200px;">
+      <div class="container" style="padding-left:5%;">
+        <div class="row">
+          <div class="col s10" style="margin:4.4%; background-color:rgba(0, 0, 0, 0.8);">
+            <form class="col s12">
+              <h4 style="text-align:center; color:white" ><b>Select room</b> </h4>
+              <hr>
+              <div class="row">
+                <div class="input-field col s12 " style="padding: 0 0 0 0px;">
+                  <a class="waves-effect waves-light btn btn-block grey darken-3" style="margin: 0 0 0 0; width:100%;">Room 1</a>
+                </div>
+              </div>
+              <br>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #ffffff;
+}
+
+.body-background{
+  text-align: left;
+  background-image: url('http://epicwallpaperz.com/wallpaper-hd/ww2-tank-wallpaper-phone-On-wallpaper-hd.jpg');
+  height: 100%;
+  width: 100%;
+  background-size: cover;
+  position: absolute;
+}
+.modal {
+  overflow: hidden;
+  width: 35%;
+  height: 600px;
+  margin-top: 0px;
+  padding: 0 0 0 0;
+}
+
+h5
+{
+	font-weight: 400;
+}
+
+.container{
+	margin: 0 0 0 0;
+	width: 455px;
+	height: 100%;
+}
+
+.tabs .indicator
+{
+	background-color: #1e2121;
+	opacity: 0.3;
+}
+
+.form-container
+{
+	padding: 40px;
+	padding-top: 10px;
+}
+
+.confirmation-tabs-btn
+{
+	position: absolute;
+}
+element.style {
+    z-index: 1003;
+    display: block;
+    opacity: 1;
+    top: 1%;
+}
+</style>
